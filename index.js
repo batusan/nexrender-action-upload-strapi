d4ec146f1670f4ba3580a6107c6e8e9696abce20
Batuhan San <batuhansann@gmail.com>
2023-04-29 20:09:47 +0300

    Added functionality

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // index.js
-module.exports = (job, settings, action, type) => {
-  console.log("hello from my action: " + action);
-  console.log("job : ", job);
-  console.log("settings", settings);
-  console.log("type : ", type);
-  return Promise.resolve();
+module.exports = (job, settings, { input, api_url, folderId }, type) => {
+  if (type != "postrender") {
+    throw Error("this action only can be used in postrender.");
+  }
+  return Promise((resolve, reject) => {
+    const headers = {
+      "Content-Type": "multipart/form-data",
+    };
+
+    const formData = new FormData();
+    formData.append("files", fs.createReadStream(input));
+    formData.append("ref", "media");
+    formData.append("field", "media");
+    formData.append("refId", folderId);
+
+    axios
+      .post(api_url, formData, { headers: headers })
+      .then((res) => {
+        resolve(`Upload success. File path: ${res.data[0].url}`);
+      })
+      .catch((err) => {
+        reject(`Upload failed. Error: ${err.message}`);
+      });
+  });
 };
