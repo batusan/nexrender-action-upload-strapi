ac161ecb581dfc0155f9c47c2c6cbf32b5dbf112
Batuhan San <batuhansann@gmail.com>
2023-04-29 19:32:16 +0300

    Debug

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index.js
 module.exports = (job, settings, action, type) => {
-    console.log('hello from my module: ' + action.module);
-    return Promise.resolve();
-}
+  console.log("hello from my action: " + action);
+  console.log("job : ", job);
+  console.log("settings", settings);
+  console.log("type : ", type);
+  return Promise.resolve();
+};
